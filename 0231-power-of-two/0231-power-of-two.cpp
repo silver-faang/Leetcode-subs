9e0ee0aabd293b05eba59c857fc55ff5e3d9c8c1
silver-faang <83732168+silver-faang@users.noreply.github.com>
2025-08-09 20:09:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.2%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==0)return false;
-        while(n%2==0){
-            n/=2;
-
-        }
-        if(n==1) return true;
-        return false;
+        return n>0 && (n & n-1)==0;
     }
 };
