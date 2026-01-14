69c7d7e31590a7da2be15f80c745ec43d7412294
silver-faang <83732168+silver-faang@users.noreply.github.com>
2026-01-14 21:24:57 +0530

    Time: 75 ms (87.79%), Space: 198.7 MB (52.33%) - LeetHub

--- 3453-separate-squares-i/3453-separate-squares-i.cpp
+++ 3453-separate-squares-i/3453-separate-squares-i.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     double separateSquares(vector<vector<int>>& squares) {
-        double max_y = 0, total_area = 0;
-        for (auto& sq : squares) {
-            int y = sq[1], l = sq[2];
-            total_area += (double)l * l;
-            max_y = max(max_y, (double)(y + l));
+        double ymx=0,ta=0;
+        for(auto& sq:squares){
+            int y=sq[1],l=sq[2];
+            ta+=(double)l*l;
+            ymx=max(ymx,double(y+l));
         }
-
-        auto check = [&](double limit_y) -> bool {
-            double area = 0;
-            for (auto& sq : squares) {
-                int y = sq[1], l = sq[2];
-                if (y < limit_y) {
-                    area += l * min(limit_y - y, (double)l);
-                }
+        auto chk=[&](double y)->bool{
+            double ar=0;
+            for(auto& sq:squares){
+                int y1=sq[1],l1=sq[2];
+                if(y1<y)
+                 ar+=l1*min(y-y1,(double)l1);
             }
-            return area >= total_area / 2;
+            return ar>=ta/2;
         };
-
-        double lo = 0, hi = max_y;
-        double eps = 1e-5;
-        while (abs(hi - lo) > eps) {
-            double mid = (hi + lo) / 2;
-            if (check(mid)) {
-                hi = mid;
-            } else {
-                lo = mid;
+        double lo=0,hi=ymx;
+        double eps=1e-5;
+        while(abs(hi-lo)>=eps){
+            double md=(hi+lo)/2;
+            if(chk(md)){
+                hi=md;
             }
+            else lo=md;
         }
         return hi;
     }
