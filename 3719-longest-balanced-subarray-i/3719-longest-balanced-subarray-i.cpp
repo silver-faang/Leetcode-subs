bd06f8f8b0352d94cd8985c84315e4db84f1592d
silver-faang <83732168+silver-faang@users.noreply.github.com>
2026-02-11 10:29:22 +0530

    Time: 58 ms (98.14%), Space: 35 MB (100%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,18 +1,20 @@
+constexpr int N=1e5+1;
+short freq[N]={0};
 class Solution {
 public:
-    int longestBalanced(vector<int>& nums) {
-        int l=0;
-        for(int i=0;i<nums.size();i++){
-            auto odd=unordered_map<int,int>();
-            auto even=unordered_map<int,int>();
-            for(int j=i;j<nums.size();j++){
-                auto& c=(nums[j]&1)?odd:even;
-                c[nums[j]]++;
-                if(odd.size()==even.size()){
-                    l=max(l,j-i+1);
-                }
+    static int longestBalanced(vector<int>& nums) {
+        const int n=nums.size();
+        int len=0;
+        for(int l=0; l<n; l++){
+            int cnt[2]={0};
+            for(int r=l; r<n; r++){
+                const int x=nums[r];
+                if (++freq[x]==1) cnt[x&1]++;
+                if (cnt[0]==cnt[1])
+                    len=max(len, r-l+1);
             }
+            for(int i=l; i<n; i++) freq[nums[i]]=0;
         }
-        return l;
+        return len;
     }
 };
