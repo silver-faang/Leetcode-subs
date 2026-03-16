5bc3fad560653cbbf2007b3c4a85fadd781c262e
silver-faang <83732168+silver-faang@users.noreply.github.com>
2026-03-17 01:01:21 +0530

    Time: 31 ms (28.72%), Space: 17.4 MB (48.93%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,9 +3,6 @@ public:
     int numIslands(vector<vector<char>>& grid) {
         if (grid.empty()) return 0;
         int n = grid.size(), m = grid[0].size();
-        
-        // Use std::function to allow the lambda to call itself recursively
-        // or declare the lambda and pass a reference to itself.
         vector<vector<int>> vis(n, vector<int>(m, 0));
         
         function<void(int, int)> dfs = [&](int i, int j) {
@@ -16,8 +13,6 @@ public:
             for (int k = 0; k < 4; k++) {
                 int ni = i + dx[k];
                 int nj = j + dy[k];
-                
-                // Boundary check MUST happen before accessing the grid or vis matrix
                 if (ni >= 0 && ni < n && nj >= 0 && nj < m && 
                     !vis[ni][nj] && grid[ni][nj] == '1') {
                     dfs(ni, nj);
